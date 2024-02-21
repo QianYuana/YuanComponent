14b3d13766bacb5569f6ed00ffc3c21772ef9def
qingui <qingui@dianjia.io>
2024-02-21 13:54:49 +0800

    添加键盘事件

--- QianYuanX/src/Components/keyInput/index.tsx
+++ QianYuanX/src/Components/keyInput/index.tsx
@@ -1,30 +1,70 @@
-import React,{useState,useEffect} from "react";
-import { Select } from "antd";
-
+import { Button, Input, Select } from 'antd';
+import * as React from 'react';
+import { useState } from 'react';
 interface IAppProps {
-  option: any;
-  onChange?: any;
+  onAdd: (value: object) => void;
 }
 
 const App: React.FunctionComponent<IAppProps> = (props) => {
-  const [items,setItems]=useState<any[]>([])
-  const filiter = (option: any[]): any[] => {
-    let arr=option.map((item: any) => {
-     return {value:item.code,label:item.name}
-    });
-    return arr;
+  const [data, setdata] = useState(''); //输入框的值
+  const [sel, setSel] = useState('Single'); //选择器的值
+  const handleChange = (value: string) => {
+    console.log(`selected ${value}`);
+    if (value !== sel) {
+      setdata('');
+    }
+    setSel(value);
   };
-  useEffect(() => {
-    setItems(filiter(props.option));
-  }, [props.option]);
+  const add = () => {
+    props.onAdd({ data, sel });
+  };
+  const handleKeyPress = (event: { key: string }) => {
+    console.log(event.key);
+    if (event.key === 'Enter') {
+      // 在这里执行你想要的逻辑，比如提交表单或者进行搜索
+      console.log('Enter key pressed');
+      add();
+    }
+  };
   return (
-    <Select
-      defaultValue={items[0]}
-      style={{ width: 120 }}
-      onChange={props.onChange}
-      options={items}
-      {...props}
-    />
+    <>
+      <Select
+        style={{ width: 120 }}
+        onChange={handleChange}
+        value={sel}
+        options={[
+          {
+            label: '单码',
+            value: 'Single',
+          },
+          {
+            label: '整码',
+            value: 'Whole',
+          },
+        ]}
+      />
+      &nbsp;
+      <Input
+        placeholder="商品码"
+        value={data}
+        style={{ width: '180px' }}
+        onChange={(e) => setdata(e.target.value)}
+        onKeyPress={handleKeyPress}
+      />
+      <Button
+        type="primary"
+        onClick={add}
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          fontSize: 14,
+          fontWeight: 700,
+          margin: '0 4px',
+        }}
+      >
+        +
+      </Button>
+    </>
   );
 };
 
