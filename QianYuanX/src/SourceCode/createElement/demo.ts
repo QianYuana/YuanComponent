feabfa453c5202dbc64268b4faacda6e4688db0f
qiangui <qingui@dianjia.io>
2024-07-08 17:53:45 +0800

    打包的界面

--- QianYuanX/src/SourceCode/createElement/demo.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-const app= ()=>{
-    const b = React.createElement('div', { data: '1234' }, 'oneTwo');
-    console.log(b);
-    return  b
-}
-export default app
---
+++ docs-dist/docs__index.md.6fe85b03.async.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkqianyuanx=self.webpackChunkqianyuanx||[]).push([[6935],{36680:function(a,e,_){_.r(e);var o=_(2143),m=_(93359),i=_(19977),s=_(50569),E=_(24268),D=_(96057),l=_(88947),d=_(73847),C=_(67294),n=_(85893);function u(){var t=(0,d.eL)(),M=t.texts;return(0,n.jsx)(d.dY,{children:(0,n.jsx)(n.Fragment,{})})}e.default=u}}]);
