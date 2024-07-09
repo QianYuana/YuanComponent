97fd570543dcf4df03073140e86d3fa1901ddf08
qiangui <qingui@dianjia.io>
2024-07-09 10:31:32 +0800

    提交

--- QianYuanX/.dumirc.ts
+++ QianYuanX/.dumirc.ts
@@ -2,8 +2,11 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   outputPath: 'docs-dist',
+  base: '/QianYuanX/docs-dist/',
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/QianYuanX/docs-dist/' : '/',
   resolve: {
-    // 设置横向导航栏  
+    // 设置横向导航栏
     atomDirs: [
       { type: 'components', dir: 'src/Components' }, // 组件导航
       { type: 'Hooks', dir: 'src/Hooks' }, // hooks导航
@@ -18,8 +21,13 @@ export default defineConfig({
       github: 'https://github.com/QianYuana/YuanComponent.git',
     },
     // editLink:true,
-    editLink: 'https://github.com/QianYuana/YuanComponent/tree/master/QianYuanX/{filename}',
-    footer: "<span><img src='/images/logo1.svg'/><b>千源星</b><span class='footer-span'>|</span><b>作者：贾梦源</b></span><br><span>Copyright © 2023-present<span> "
+    editLink:
+      'https://github.com/QianYuana/YuanComponent/tree/master/QianYuanX/{filename}',
+    footer: `<span><img src='${
+      process.env.NODE_ENV === 'production'
+        ? '/QianYuanX/docs-dist/images/logo1.svg'
+        : '/images/logo1.svg'
+    }'/><b>千源星</b><span class='footer-span'>|</span><b>作者：贾梦源</b></span><br><span>Copyright © 2023-present<span> `,
   },
   locales: [
     { id: 'zh-CN', name: '中文' },
@@ -29,25 +37,35 @@ export default defineConfig({
     // 完整地址
     // 'https://domain.com/favicon.ico',
     // 此时将指向 `/favicon.png` ，确保你的项目含有 `public/favicon.png`
-    '/images/logo1.svg'
+    process.env.NODE_ENV === 'production'
+      ? '/QianYuanX/docs-dist/images/logo1.svg'
+      : '/images/logo1.svg',
   ],
   proxy: {
     '/api': {
-      'target': 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=3a744b62-1c69-405e-bf6e-8a916f70d62b',
-      'changeOrigin': true,
-      'pathRewrite': { '^/api' : '' },
-    }
+      target:
+        'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=3a744b62-1c69-405e-bf6e-8a916f70d62b',
+      changeOrigin: true,
+      pathRewrite: { '^/api': '' },
+    },
   },
-  logo: '/images/logo1.svg',
-  styles: [`.dumi-default-logo {font-family: Alibaba-PuHuiTi, 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;}
-  `, `.dumi-default-header-right {
+  logo:
+    process.env.NODE_ENV === 'production'
+      ? '/QianYuanX/docs-dist/images/logo1.svg'
+      : '/images/logo1.svg',
+  styles: [
+    `.dumi-default-logo {font-family: Alibaba-PuHuiTi, 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;}
+  `,
+    `.dumi-default-header-right {
     justify-content: end !important;
-  }`, `
+  }`,
+    `
   .dumi-default-header-right-aside {
     display: flex;
     align-items: center;
     margin-left: 40px;
-  }`, `.dumi-default-header-content {
+  }`,
+    `.dumi-default-header-content {
     display: flex;
     align-items: center;
     margin: 0 auto;
@@ -55,14 +73,16 @@ export default defineConfig({
     max-width: 1620px !important;
     height: 76px;
     box-sizing: border-box;
-}`, `.dumi-default-doc-layout > main {
+}`,
+    `.dumi-default-doc-layout > main {
   display: flex;
   align-items: flex-start;
   margin: 0 auto;
   padding: 0 24px;
   max-width: 1620px !important;
   box-sizing: border-box;
-}`, `.dumi-default-sidebar {
+}`,
+    `.dumi-default-sidebar {
   position: sticky;
   top: 76px;
   width: auto !important;
@@ -80,5 +100,11 @@ export default defineConfig({
 }`,
     `.dumi-default-features[data-cols='2'] > .dumi-default-features-item:nth-child(odd) {
   margin-inline-end: 0 !important; 
-}`, `/less/index.less`],
+}`,
+    `${
+      process.env.NODE_ENV === 'production'
+        ? '/QianYuanX/docs-dist/less/index.less'
+        : '/less/index.less'
+    }`,
+  ],
 });
